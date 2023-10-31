7b147582f97cefa29f8ace6f55923aa548be6ff6
Zstzbxn <zstzbxn@pop-os.localdomain>
2023-10-31 12:39:54 +0100

    edit Makefile, Multi Handler Invariants, mainnet forked tests

--- backend/test/fuzz/ActorManager.t.sol
+++ backend/test/fuzz/ActorManager.t.sol
@@ -17,53 +17,53 @@ contract ActorManager is CommonBase, StdCheats, StdUtils {
     }
 
     function mint(uint256 handlerIndex, address imageAddress, address token) external {
-        handlerIndex = bound(handlerIndex, 0, handlers.length);
+        handlerIndex = bound(handlerIndex, 0, handlers.length - 1);
         handlers[handlerIndex].mint(imageAddress, token);
     }
 
     function lockImage(uint256 handlerIndex, address imageAddress) external {
-        handlerIndex = bound(handlerIndex, 0, handlers.length);
+        handlerIndex = bound(handlerIndex, 0, handlers.length - 1);
         handlers[handlerIndex].lockImage(imageAddress);
     }
 
     function unlockImage(uint256 handlerIndex) external {
-        handlerIndex = bound(handlerIndex, 0, handlers.length);
+        handlerIndex = bound(handlerIndex, 0, handlers.length - 1);
         handlers[handlerIndex].unlockImage();
     }
 
     function confirmOrder(uint256 handlerIndex) external {
-        handlerIndex = bound(handlerIndex, 0, handlers.length);
+        handlerIndex = bound(handlerIndex, 0, handlers.length - 1);
         handlers[handlerIndex].confirmOrder();
     }
 
     function clearOrderId(uint256 handlerIndex) external {
-        handlerIndex = bound(handlerIndex, 0, handlers.length);
+        handlerIndex = bound(handlerIndex, 0, handlers.length - 1);
         handlers[handlerIndex].clearOrderId();
     }
 
     function mintCertificate(uint256 handlerIndex) external {
-        handlerIndex = bound(handlerIndex, 0, handlers.length);
+        handlerIndex = bound(handlerIndex, 0, handlers.length - 1);
         handlers[handlerIndex].mintCertificate();
     }
 
     // /* ===== Owner Functions ===== */
 
     function withdrawToken(uint256 handlerIndex, address token) external {
-        handlerIndex = bound(handlerIndex, 0, handlers.length);
+        handlerIndex = bound(handlerIndex, 0, handlers.length - 1);
         handlers[handlerIndex].withdrawToken(token);
     }
 
     // /* ===== Admin Functions ===== */
 
     function setPrinted(uint256 handlerIndex) external {
-        handlerIndex = bound(handlerIndex, 0, handlers.length);
+        handlerIndex = bound(handlerIndex, 0, handlers.length - 1);
         handlers[handlerIndex].setPrinted();
     }
 
     // /* ===== Helper Functions ===== */
 
     // function updateTimestamp(uint256 handlerIndex) public {
-    //    handlerIndex = bound(handlerIndex, 0, handlers.length);
+    //    handlerIndex = bound(handlerIndex, 0, handlers.length - 1);
     //    handlers[handlerIndex].updateTimestamp();
     // }
 }
