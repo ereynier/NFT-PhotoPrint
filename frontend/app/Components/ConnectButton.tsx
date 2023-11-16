32dcb39b6d1354dcf33a7cd39d7980c765447b30
Zstzbxn <zstzbxn@pop-os.localdomain>
2023-11-16 11:33:16 +0100

    upgrade copied feedback

--- frontend/app/Components/ConnectButton.tsx
+++ frontend/app/Components/ConnectButton.tsx
@@ -10,19 +10,26 @@ import {
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import ConnectDialog from './ConnectDialog'
-import { Badge } from '@/components/ui/badge'
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
 
 const ConnectButton = () => {
 
     const { address, isConnected } = useAccount()
     const [animation, setAnimation] = React.useState('opacity-0')
+    const [copiedStatus, setCopiedStatus] = React.useState(false)
 
     const onClickCopy = () => {
         navigator.clipboard.writeText(address as string);
-        setAnimation('opacity-0 translate-y-0');
-        setAnimation('opacity-100 translate-y-8 transition-all duration-1000');
+    }
+
+    const copiedPopoverChanged = () => {
+        setCopiedStatus(true)
         setTimeout(() => {
-            setAnimation('opacity-0 translate-y-0');
+            setCopiedStatus(false)
         }, 1000)
     }
 
@@ -31,10 +38,14 @@ const ConnectButton = () => {
             {isConnected ? (
                 <div className='flex flex-row gap-2'>
                     <p title={`${address}`} className={`break-all text-black font-medium text-lg cursor-default`}>{`${address?.slice(0, 5)}...${address?.slice(-4)}`}</p>
-                    <button onClick={() => onClickCopy()} title='Click to copy' className='hover:bg-input rounded-md flex items-center justify-center p-1'>
-                        <Image src="/copy.svg" alt="Copy" width={20} height={20} />
-                        <Badge variant={"outline"} className={`absolute ${animation}`}>Copied</Badge>
-                    </button>
+                    <Popover open={copiedStatus} onOpenChange={() => copiedPopoverChanged()}>
+                        <PopoverTrigger asChild>
+                            <button onClick={() => onClickCopy()} title='Click to copy' className='hover:bg-input rounded-md flex items-center justify-center p-1'>
+                                <Image src="/copy.svg" alt="Copy" width={20} height={20} />
+                            </button>
+                        </PopoverTrigger>
+                        <PopoverContent className='w-fit p-1'>Copied</PopoverContent>
+                    </Popover>
                 </div>
             ) : (
                 // Dialog with a list of connectors
