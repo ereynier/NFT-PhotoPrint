f77b3dd570feee1af5173dacce3dde8f521fd158
Zstzbxn <zstzbxn@pop-os.localdomain>
2023-11-29 16:28:14 +0100

    contact page + license

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
 
 export default function Home() {
-    return (
-      <div className="dark:dark text-foreground bg-background">
-        Contact
+  return (
+    <div className="m-auto h-screen w-full flex items-center justify-center">
+      <div className="h-fit w-fit flex flex-col gap-16 items-center">
+        <h1 className='text-6xl font-semibold'>Contact</h1>
+        <div className="h-fit w-full flex flex-col gap-5 items-center text-lg sm:text-xl md:text-2xl">
+          <p className="">Mail: <Link href="mailto:esteban@ereynier.me" className="underline">esteban@ereynier.me</Link></p>
+          <p className="">Linkedin: <Link href="https://linkedin.com/in/ereynier" target="_blank" className="underline">@ereynier</Link></p>
+        </div>
       </div>
-    )
-  }
-  
+    </div>
+  )
+}
--- frontend/app/terms/page.tsx
+++ frontend/app/terms/page.tsx
@@ -1,9 +1,42 @@
 
 export default function Home() {
-    return (
-      <div className="dark:dark text-foreground bg-background">
-        Terms
-      </div>
-    )
-  }
-  
+  return (
+    <div className="max-w-3xl mx-auto my-8">
+      <h1 className="text-3xl font-bold mb-4">Terms and Conditions</h1>
+
+      <h2 className="text-xl font-bold mb-2">1. Acceptance of Terms</h2>
+      <p className="mb-4">
+        By accessing this website, you agree to be bound by these terms and conditions, all applicable laws, and regulations, and agree that you are responsible for compliance with any applicable local laws.
+      </p>
+
+      <h2 className="text-xl font-bold mb-2">2. Intellectual Property</h2>
+      <p className="mb-4">
+        All content on this website, including but not limited to text, images, graphics, and code, is the property of Esteban Reynier and is protected by intellectual property laws.
+      </p>
+
+      <h2 className="text-xl font-bold mb-2">3. NFT Purchases</h2>
+      <p className="mb-4">
+        When purchasing NFTs from this website, you acknowledge that NFT ownership is transferred on the blockchain. The terms of the smart contract and any additional terms specified during the purchase process apply.
+      </p>
+
+      <h2 className="text-xl font-bold mb-2">4. Fine Art Prints</h2>
+      <p className="mb-4">
+        The purchase of an NFT may include the option of requesting a physical art print. Printing and delivery are managed by an external company.
+      </p>
+
+      <h2 className="text-xl font-bold mb-2">5. Fine Art Prints</h2>
+      <p className="mb-4">
+        The purchase of an NFT may include the option to claim a physical fine art print as a bonus. Please note that the inclusion of fine art prints as a bonus is discretionary and may be subject to change. Even if an NFT has already been minted, the availability of the print bonus can be stopped at any time by the creator.
+      </p>
+
+      <h2 className="text-xl font-bold mb-2">6. Limited Edition</h2>
+      <p className="mb-4">
+        NFTs and fine art prints from this collection are part of a limited edition. The total number of editions and any variations are specified in the individual listings.
+      </p>
+
+      <p className="mt-8">
+        For any inquiries regarding these terms and conditions, please contact us at <a href="mailto:contact@ereynier.me" className="font-bold">contact@ereynier.me</a>.
+      </p>
+    </div>
+  )
+}
